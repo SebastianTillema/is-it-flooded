c2ee0d1d50592405c00f0a7aa22fa8e9954b23b5
SebastianTillema <strasmussen8@gmail.com>
2025-01-06 17:07:45 +0100

    update to angular 18

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule),
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(),
   ]
 }).catch(err => console.error(err));

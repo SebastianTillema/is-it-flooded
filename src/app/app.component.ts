670d1f93ec35ddef444f3ca199aad79ad9279054
SebastianTillema <strasmussen8@gmail.com>
2025-01-07 16:15:21 +0100

    smooth wave animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase, NgIf, NgFor, JsonPipe } from '@angular/common';
 import { SensorDataServiceService } from './sensor-data-service.service';
 import { SensorData, getWaterLevel } from './sensor-data';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,15 +14,23 @@ import { SensorData, getWaterLevel } from './sensor-data';
 export class AppComponent implements OnInit {
   waterlevel: number | undefined;
   flooded: boolean | undefined;
-  topHeight = '450px'; // Dynamic height value
-  constructor(private sensorService: SensorDataServiceService) {
-    document.documentElement.style.setProperty('--top-height', this.topHeight);
-  }
+  height = 10; // Dynamic height value
 
+  constructor(private sensorService: SensorDataServiceService, private sanitizer: DomSanitizer) {
+  }
+  get style() {
+    return this.sanitizer.bypassSecurityTrustStyle(`--height: ${this.height}%`);
+  }
   ngOnInit() {
     this.sensorService.getSensorData().subscribe((data) => {
       this.waterlevel = getWaterLevel(data);
       this.flooded = this.waterlevel > 1.40;
+      this.height = 90
     });
   }
+
+  inc() {
+    this.height -= 10;
+  }
+
 }

3a9c6b8f4cc8a5242a8d6736a44a55bb44041cca
SebastianTillema <strasmussen8@gmail.com>
2025-01-10 20:22:42 +0100

    redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,33 +4,25 @@ import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase, NgIf, NgFor, JsonPipe } from '@angular/common';
 import { SensorDataServiceService } from './sensor-data-service.service';
 import { SensorData, getWaterLevel } from './sensor-data';
+import { WaveBackgroundComponent } from "../wave-background/wave-background.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet, NgIf, NgFor, JsonPipe]
+  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet, NgIf, NgFor, JsonPipe, WaveBackgroundComponent]
 })
 export class AppComponent implements OnInit {
   waterlevel: number | undefined;
   flooded: boolean | undefined;
-  height = 10; // Dynamic height value
 
-  constructor(private sensorService: SensorDataServiceService, private sanitizer: DomSanitizer) {
+  constructor(private sensorService: SensorDataServiceService) {
   }
-  get style() {
-    return this.sanitizer.bypassSecurityTrustStyle(`--height: ${this.height}%`);
-  }
+
   ngOnInit() {
     this.sensorService.getSensorData().subscribe((data) => {
       this.waterlevel = getWaterLevel(data);
       this.flooded = this.waterlevel > 1.40;
-      this.height = 90
     });
   }
-
-  inc() {
-    this.height -= 10;
-  }
-
 }
